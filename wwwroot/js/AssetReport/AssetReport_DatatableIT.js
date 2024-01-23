bf0e32e387b818a68abd122a184094adebac0c32
Christian VIllamer <36148483+cvillamer13@users.noreply.github.com>
2024-01-23 14:04:20 +0800

    IT Asset

---
+++ wwwroot/js/AssetReport/AssetReport_DatatableIT.js
@@ -0,0 +1,100 @@
+﻿$(document).ready(function () {
+    document.title = 'Asset Report';
+
+    $("#tblAsset_report").DataTable({
+
+        paging: true,
+        select: true,
+        "order": [[0, "desc"]],
+        dom: 'Bfrtip',
+
+
+        buttons: [
+            'pageLength',
+            'print',
+            'csv'
+        ],
+
+
+        "processing": true,
+        "serverSide": true,
+        "filter": false, //Search Box
+        "orderMulti": true,
+        "stateSave": true,
+
+        "ajax": {
+            "url": "/AssetReport/GetDataTabelDataIT",
+            "type": "POST",
+            "datatype": "json"
+
+        },
+        "columns": [
+            {
+
+
+                data: "Id", "name": "Id", render: function (data, type, row) {
+
+                    return "<a href='#' class='fa fa-eye' onclick=Details('" + row.Id + "');>" + row.Id + "</a>";
+                }
+            },
+
+            {
+                data: "Image", "name": "Image", render: function (data, type, row) {
+
+                    return "<img src=" + row.Image + " class='img - circle elevation - 2 imgCustom75px' alt='Asset Image'>";
+                }
+            },
+            { "data": "AssetId", "name": "AssetId" },
+            { "data": "AssetModelNo", "name": "AssetModelNo" },
+            { "data": "Name", "name": "Name" },
+            { "data": "Description", "name": "Description" },
+            { "data": "Quantity", "name": "Quantity" },
+            { "data": "UnitName", "name": "UnitName" },
+            { "data": "AssetStatusName", "name": "AssetStatusName" },
+            {
+
+                "data": "DateOfPurchase",
+                "name": "DateOfPurchase",
+                "autoWidth": true,
+                "render": function (data) {
+
+
+                    var date = new Date(data);
+
+                    var month = date.getMonth() + 1;
+
+                    var FormattedDate = (month.length > 1 ? month : month) + "/" + date.getDate() + "/" + date.getFullYear();
+
+                    if (FormattedDate == "1/1/1970") {
+                        return "N/A"
+                    } else {
+                        return FormattedDate
+                    }
+
+                }
+            },
+            { "data": "LocationName", "name": "LocationName" },
+            { "data": "AreaName", "name": "AreaName" },
+            { "data": "SubAreaName", "name": "SubAreaName" },
+            { "data": "CategoryName", "name": "CategoryName" },
+            { "data": "SubCategoryName", "name": "SubCategoryName" },
+            { "data": "SupplierName", "name": "SupplierName" },
+
+            { "data": "CompanyName", "name": "CompanyName" },
+            { "data": "DepartmentName", "name": "DepartmentName" },
+            { "data": "SubDepartmentName", "name": "SubDepartmentName" },
+
+
+
+
+        ],
+        //'columnDefs': [{
+        //    'targets': [44, 55],
+        //    'orderable': false,
+        // }],
+
+        "lengthMenu": [[10, 20, 30, 40, 50, 100, 1000], [10, 20, 30, 40, 50, 100, 1000]]
+    });
+
+});
+
---
+++ wwwroot/js/AssetScanned/AssetScanned_DatatableIT.js
@@ -0,0 +1,69 @@
+﻿$(document).ready(function () {
+    document.title = 'Asset IT Scanned';
+
+    $("#tblAsset_report").DataTable({
+
+        paging: false,
+        select: true,
+        "order": [[0, "desc"]],
+        dom: 'Bfrtip',
+
+        "processing": false,
+        "serverSide": true,
+        "filter": false, //Search Box
+        "orderMulti": false,
+        "stateSave": false,
+
+        "ajax": {
+            "url": "/AssetScanned/GetDataTabelDataIT",
+            "type": "POST",
+            "datatype": "json"
+
+        },
+        "columns": [
+
+            { "data": "Id", "name": "Id" },
+            { "data": "AssetId", "name": "AssetId" },
+            { "data": "AssetModelNo", "name": "AssetModelNo" },
+            { "data": "Name", "name": "Name" },
+            { "data": "Description", "name": "Description" },
+            { "data": "Quantity", "name": "Quantity" },
+            { "data": "AssetStatusName", "name": "AssetStatusName" },
+            { "data": "AssetDateScanned", "name": "AssetDateScanned" },
+            { "data": "AssetTimeScanned", "name": "AssetTimeScanned" },
+            {
+                "data": "AssetDateScanned",
+                "name": "AssetDateScanned",
+                "autoWidth": true,
+                "render": function (data, type, row) {
+                    // Assuming data is in a format like "MM/dd/yyyy"
+                    var date = new Date(data);
+                    var today = new Date(); // Current date
+
+                    // Compare the date with today's date
+                    if (date.setHours(0, 0, 0, 0) === today.setHours(0, 0, 0, 0)) {
+                        // If the date is today, set background color to green
+                        return '<div style="background-color: green; color: white;"> Scanned <input type="hidden" name="TheScanner" value="S" /> </div>';
+                    } else {
+                        // Otherwise, set background color to the default
+                        return '<div style="background-color: red; color: white;"> Not Scanned  <input type="hidden" name="TheScanner" value="NS" /> </div>';
+                    }
+                }
+            },
+
+
+
+
+
+
+
+        ],
+
+    });
+
+
+
+
+
+});
+
